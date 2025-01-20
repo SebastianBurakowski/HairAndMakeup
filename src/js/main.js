5aa442e6d8ebad3ada46853777f27c28e45638b8
Sebastian Burakowski <s-burakowski@wp.pl>
2025-01-20 15:47:35 +0100

    about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 let aboutSection;
+let sectionImg;
+let sectionTitle;
+let sectionText;
 
 
 const prepareDomElements = () => {
@@ -7,17 +10,37 @@ const prepareDomElements = () => {
 
 
     aboutSection = document.querySelector('.about')
+    sectionImg = document.querySelector('.about__img')
+    sectionTitle = document.querySelector('.about__heading')
+    sectionText = document.querySelector('.about__text')
 
 }
 
 //FUNCTIONS
 
+const handleShowAbout = () => {
+
+    const aboutSectionTop = aboutSection.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if(aboutSectionTop <= windowHeight * 0.6){
+        sectionImg.classList.add('activeAnimation')
+        sectionTitle.classList.add('activeAnimation')
+        sectionText.classList.add('activeAnimation')
+    } else {
+        sectionImg.classList.remove('activeAnimation')
+        sectionTitle.classList.remove('activeAnimation')
+        sectionText.classList.remove('activeAnimation')
+    }
+
+    
+}
 
 
 const prepareDomEvents = () => {
 
     ////////// WYWOŁANIE FUNKCJI ///////////////////
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleShowAbout);
 
 }
 
