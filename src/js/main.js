4ac87e6789427758ebd614a42eb35d3b507aa4a4
Sebastian Burakowski <s-burakowski@wp.pl>
2025-01-20 16:27:31 +0100

    hero section

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ let aboutSection;
 let sectionImg;
 let sectionTitle;
 let sectionText;
+let heroSection;
+let heroVideos;
 
 
 const prepareDomElements = () => {
@@ -13,15 +15,17 @@ const prepareDomElements = () => {
     sectionImg = document.querySelector('.about__img')
     sectionTitle = document.querySelector('.about__heading')
     sectionText = document.querySelector('.about__text')
+    heroSection = document.querySelector('.hero')
+    heroVideos = document.querySelectorAll('.hero__video')
 
 }
-
+const windowHeight = window.innerHeight;
 //FUNCTIONS
 
 const handleShowAbout = () => {
 
     const aboutSectionTop = aboutSection.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+    
 
     if(aboutSectionTop <= windowHeight * 0.6){
         sectionImg.classList.add('activeAnimation')
@@ -36,11 +40,27 @@ const handleShowAbout = () => {
     
 }
 
+const handleShowHero = () => {
+    const heroSectionTop = heroSection.getBoundingClientRect().top;
+
+    if(heroSectionTop <= windowHeight * 0.7){
+        heroVideos.forEach(video => {
+            video.classList.add('activeVideo')
+        })
+    } else {
+        heroVideos.forEach(video => {
+            video.classList.remove('activeVideo')
+        })
+    }
+    
+}
+
 
 const prepareDomEvents = () => {
 
     ////////// WYWOŁANIE FUNKCJI ///////////////////
     window.addEventListener("scroll", handleShowAbout);
+    window.addEventListener("scroll", handleShowHero);
 
 }
 
